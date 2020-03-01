20a54ca5b3dee122237d1b80cbde7989aad43458
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2020-03-01 18:26:33 -0500

    Updated remote PS

--- RunPSScriptRemotely.py
+++ RunPSScriptRemotely.py
@@ -6,30 +6,28 @@ from cbapi.response import CbEnterpriseResponseAPI, Sensor, SensorGroup
 cb = CbEnterpriseResponseAPI()
 
 class RunExeRemotely(object):
-    def __init__(self, HostName, ToolName, Commandline,code,OutputDir, OutputExtension):
+    def __init__(self, HostName,scriptname,code,OutputDir, OutputExtension):
             self.HostName = HostName
-            self.ToolName=ToolName
-            self.Commandline=Commandline
+            self.scriptname=scriptname
             self.OutputDir=OutputDir
             self.OutputExtension=OutputExtension
             self.code=code
             
     # sensor_id = 150  # Use this to define the sensor ID in the script, rather than using input
-    def RunExe (self, session):
+    def RunCode (self, session):
         HostName=self.HostName
-        Tool=self.ToolName
-        Commandline=self.Commandline
+        script=self.scriptname
 
-        localpath=os.path.join('Tools',Tool)
+        localpath=os.path.join('PSScripts',script)
         remotedir=r'C:\Windows\CarbonBlack\Tools'
-        remotepath=remotedir+"\\"+Tool
+        remotepath=remotedir+"\\"+script
 
         Output=self.OutputExtension
         OutputDir=self.OutputDir
         outputfile=os.path.join(OutputDir,HostName+Output)
-        command=remotepath+" "+Commandline
+        command=r"powershell.exe "+remotepath+" -executionpolicy bypass"
         code=self.code
-        #print(localpath,remotedir,remotepath,Commandline,outputfile)
+        print(command)
         try:
             try:
                 session.create_directory(remotedir)      
@@ -42,16 +40,12 @@ class RunExeRemotely(object):
                 session.put_file(open(localpath, 'rb'), remotepath)
 
             #run the powershell script and save stdout
-            #HostName=session.create_process(r'''powershell.exe $env:computername''')
-            #HostName=str(HostName).strip('b')
-            #HostName=HostName[1:-5]
             print(HostName,'tool is executing')
-            #CbLRSessionBase.create_process(command_string,wait_for_output=True,remote_output_file_name=None,working_directory=None,wait_timeout=30,wait_for_completion=True)
             output = session.create_process(command,\
-                                            wait_for_output=True,remote_output_file_name=None,working_directory=None,wait_timeout=3600,wait_for_completion=True)
+                                            wait_for_output=True,remote_output_file_name=None,working_directory=None,wait_timeout=600,wait_for_completion=True)
             #print('[SUCCESS] Script execution successful. Navigate to destination location for artifacts.')
             #print('[DEBUG] Script Output:\n\n', output)
-            print(HostName,'tool is finished')
+            #Bypass=session.create_process('powershell.exe Set-ExecutionPolicy -executionpolicy restricted -scope currentuser')
 
             #retreive the file full of autoruns
             #autoruns=session.get_file(r'C:\Windows\CarbonBlack\Tools\autoruns.csv')
@@ -76,43 +70,19 @@ class RunExeRemotely(object):
         #session.close()
         print("Session has been closed to hostname #" + HostName)
 
-##CODE STARTS HERE
-def RunAutoruns(Group):
-    #tool to run - must be in local dir
-    tool='autorunsc.exe'
-    #arguments to run the tool with
-    args=r'''-accepteula -nobanner -a * -c -h -mv -s *"'''
-    #encoding of the tools output. Most sysinternals are UTF8, autoruns is UTF16
-    code='UTF-16'
-    #dir to write the files to
-    output_dir='autorunsoutput'
-    #extension to append on hostnames for file output
-    output_ext='_autoruns.csv'
+##code starts here
 
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+def RunPowershell(Group):
+    #script to run - must be in PSScripts dir
+    script='get-autoruns.ps1'
 
-    group=cb.select(SensorGroup).where("name:"+Group).first()
-
-    for sensor in group.sensors:
-        job=RunExeRemotely(sensor.hostname,tool,args,code,output_dir,output_ext)
-        print(sensor.hostname)
-        cb.live_response.submit_job(job.RunExe, sensor)
-        print('job submitted')
-
-def RunSigCheck(Group):
-    #tool to run - must be in local dir
-    tool='sigcheck.exe'
-    #arguments to run the tool with
-    args=r'''-accepteula -nobanner -c -e -h -s "C:\windows\system32"'''
-    #encoding of the tools output. Most sysinternals are whatever the
-    #windows default is on the remote computer, autoruns is UTF16
+    #encoding of the tools output. cp1252 is the windows default
     # default can be checked with [System.Text.Encoding]::Default in powershell
     code='cp1252'
     #dir to write the files to
-    output_dir='signatureoutput'
+    output_dir='powershelloutput'
     #extension to append on hostnames for file output
-    output_ext='_signatures.csv'
+    output_ext='_psautoruns.csv'
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -120,7 +90,7 @@ def RunSigCheck(Group):
     group=cb.select(SensorGroup).where("name:"+Group).first()
 
     for sensor in group.sensors:
-        job=RunExeRemotely(sensor.hostname,tool,args,code,output_dir,output_ext)
+        job=RunExeRemotely(sensor.hostname,script,code,output_dir,output_ext)
         print(sensor.hostname)
         cb.live_response.submit_job(job.RunCode, sensor)
         print('job submitted')
@@ -128,6 +98,5 @@ def RunSigCheck(Group):
 
 #group to search on
 Group='Default Group'
-RunAutoruns(Group)
-#RunSigCheck(Group)
+RunPowershell(Group)
 
