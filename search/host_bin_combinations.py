975378e3a5e759e88dd0b33c757a69a2a4b7e4dc
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2020-06-22 08:47:37 -0400

    Update host_bin_combinations.py

--- search/host_bin_combinations.py
+++ search/host_bin_combinations.py
@@ -11,6 +11,14 @@ from cbapi.response import CbEnterpriseResponseAPI, Sensor, SensorGroup, Process
 def trim_hostname(hostname):
     return hostname.split('|')[0].upper()
 
+def trim_filename(filenames):
+    if  isinstance(filenames, str):
+        return str(filenames.split('\\')[-1].upper())
+    result = []
+    for name in filenames:
+        result.append(str(name.split('\\')[-1].upper()))
+    return result
+
 def list_to_dict(ls):
     return dict((el,1) for el in ls)
 
@@ -40,12 +48,12 @@ def obs_to_logprobabilites(obs_matrix):
         probability = np.log(total/nhosts)
         obs_matrix[:,i] *= probability
 
-#%%
 
+#%%
 cb=CbEnterpriseResponseAPI()
 
 query = cb.select(Binary)
-query = query.where('is_executable_image:true')
+query = query.where('is_executable_image:true').where('server_added_timestamp:[2020-04-01T23:59:59 TO *]')
 #query = query.where('md5:9C80EEDD823FFA6CE9CCE22BCF1C427D')
 #This runs pretty much instantly on test with 10 000 binaries in server
 bin_list = []
@@ -60,13 +68,17 @@ bin_df = pd.DataFrame(bin_list)
 bin_df_hosts = bin_df.explode('endpoint')
 
 # filter list to only binaries which are unsigned and on less than 60 hosts (arbitary)
-bin_df_hosts = bin_df_hosts.loc[bin_df_hosts['signed'] != 'Signed']
-bin_df_hosts = bin_df_hosts.loc[bin_df_hosts['host_count'] < 60]
+bin_df_hosts = bin_df_hosts.loc[((bin_df_hosts['signed'] != 'Signed') & (bin_df_hosts['host_count'] < 50))]
+
+
 
 # (convert into a df of hostname : list of binaries by both filename and md5
+bin_df_hosts['observed_filename'] = bin_df_hosts['observed_filename'].map(trim_filename)
 host_binaries = bin_df_hosts[['md5', 'endpoint']].groupby('endpoint').aggregate(lambda x: list(x))
-host_filenames = bin_df_hosts[['observed_filename', 'endpoint']].groupby('endpoint').aggregate(lambda x: list(x))
 
+
+host_filenames = bin_df_hosts[['observed_filename', 'endpoint']].groupby('endpoint').aggregate(lambda x: list(x))
+host_filenames['observed_filename'] = host_filenames['observed_filename'].map(lambda x: set([item for sublist in x for item in sublist]))
 # make a sensor query to only get hosts which have checked in within 10 days
 
 host_binaries['hostname'] = host_binaries.index.map(trim_hostname)
@@ -86,7 +98,7 @@ host_binary_observations = v.fit_transform(host_binaries['md5'])
 labels = v.get_feature_names()
 hosts = host_binaries.index
 
-#%%
+
 #take hamming distance - in this case is the number of different binaries
 hd = (2 * np.inner(host_binary_observations.T-0.5, 0.5-host_binary_observations.T) + host_binary_observations.T.shape[1] / 2)
 
@@ -101,7 +113,8 @@ corr_means=pd.DataFrame(means, index=hosts)
 # add back in binary names as a reference for the analyst
 corr_means = corr_means.join(host_filenames)
 
-#%% uncommon binaries have scores above -1
+ 
+# uncommon binaries have scores above -1
 # clf = LocalOutlierFactor(n_neighbors=3)
 # outlier_labels = clf.fit_predict(host_binary_observations.T)
 
@@ -120,7 +133,12 @@ unique_binaries = [v for i, v in enumerate(labels) if i not in to_drop]
 # go through the array and replace 1s with the binaries log frequency
 obs_to_logprobabilites(more_unique)
 
-host_probabilities = sum(more_unique.T)   
+host_probabilities = sum(more_unique.T)
+   
 
 host_probs = pd.DataFrame(host_probabilities, index=hosts)
+
+host_probs = host_probs.join(host_filenames)
+
+host_probs.to_csv('./output.csv')
 
